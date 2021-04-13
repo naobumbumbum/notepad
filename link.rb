f871f6dee3983cab9600987d9dd728a072c3506d
nao_bum_bum_bum <iamjustwayfarer@gmail.com>
2021-04-13 18:59:00 +0500

    post class + ancestors

---
+++ link.rb
@@ -0,0 +1,17 @@
+
+class Link < Post
+
+  def initialize
+    super
+
+    @url = ''
+  end
+
+  def read_from_console
+    super
+  end
+
+  def to_strings
+    super
+  end
+end
---
+++ task.rb
@@ -0,0 +1,17 @@
+
+class Task < Post
+
+  def initialize
+    super
+
+    @due_date = Time.now
+  end
+
+  def read_from_console
+    super
+  end
+
+  def to_strings
+    super
+  end
+end
